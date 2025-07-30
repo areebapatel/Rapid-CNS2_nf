ccf7c8f388c593cd975a21e8b3cb493f2a137e46
Areeba Patel <46373444+areebapatel@users.noreply.github.com>
2025-07-30 15:28:42 +0200

    Update methylation_classification.R

--- scr/methylation_classification.R
+++ scr/methylation_classification.R
@@ -1,8 +1,8 @@
 for (package in c('optparse', 'GenomicRanges','ranger','matrixStats','data.table', 'glmnet' )) {
-  if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos = "http://cran.us.r-project.org")
+  #if (!require(package, character.only=T, quietly=T)) {
+  #  install.packages(package,repos = "http://cran.us.r-project.org")
     library(package, character.only=T)
-  }
+  #}
 }
 
 #Parse arguments
